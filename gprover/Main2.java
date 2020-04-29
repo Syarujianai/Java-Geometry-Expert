e5bed2dbb50e25a7ba91a8f73e1c68b86587018b
unknown <linyiquan@dm-ai.cn>
2020-04-29 09:34:18 +0800

    feat: resolve proof step of lemma 166 (further)

--- gprover/Main2.java
+++ gprover/Main2.java
@@ -19,66 +19,149 @@ import java.util.Set;
 
 
 public class Main2 {
-    public static void viz_proof(cond pr_head, Hashtable<String, Integer> has_sd){
+    public static void viz_proof(cond pr_head, Hashtable<String, Integer> visited_pr_sd){
         cond pr = pr_head;
         if(pr.nx != null)
-            viz_proof(pr.nx, has_sd);
+            viz_proof(pr.nx, visited_pr_sd);
 
         /* visualize proof */
         int count_append = 0;
         StringBuilder builder = new StringBuilder();
-        boolean encountered_lemma = false;
-        if((pr.sd.contains("∥")) && (pr.u.pn != null) && (pr.u.pn.lemma == 166))
-            encountered_lemma = true;
+        boolean is_encounter_lemma = false, is_dup = false;
+        if((pr.sd.contains("∥")) && (pr.u.pn != null) && (pr.u.pn.lemma == 166) && (pr.vlist.size() == 2))
+            is_encounter_lemma = true;
 
-        if(encountered_lemma){
+        if(is_encounter_lemma){
+            boolean is_reverse_order = false, is_variant_type = false, is_skip_para=false;
+
+            /* get a glimpse of conclusion (already to deduct) */
+            String[] type_patterns = pr.sd.split(" ∥ ");
+            // String type_pattern_rev = new StringBuffer(type_pattern).reverse().toString();
+
+            /* prepare initial string for resolve */
             String vertex = "", angle_intermediate = "", angle_known = "";
             StringBuilder another_builder = new StringBuilder();
-            for (int i = pr.vlist.size()-1; i >= 0; i--){  // reverse
+            String[] primitives_pn = new String[]{};
+            String[] primitives_an = new String[]{};
+
+            /* detect order and type, and store primitives */
+            for (int i = 0; i < pr.vlist.size(); i++) {
                 cond v = (cond) pr.vlist.get(i);
 
-                /* resolve proof step of lemma */
-                if(v.sd.contains("∥")) {
-                    String[] symbols = v.sd.split(" ∥ ");
-                    vertex = symbols[0].substring(1);  // store vertex B (of FB) firstly
+                /* store omitted proof step of lemma */
+                if (v.sd.contains("∥")) {
+                    primitives_pn = v.sd.split(" ∥ ");
+                    if (i == 1) {
+                        is_reverse_order = true;
+                    }
+                } else if (v.sd.contains("∠")) {
+                    primitives_an = v.sd.split(" = ");
+                    angle_known = primitives_an[0];
 
-                }else if(v.sd.contains("∠")){
-                    String[] symbols = v.sd.split(" = ");
-                    angle_known = symbols[0];
-                    another_builder.append(symbols[1].substring(2, 4)).reverse();  // reverse: CD -> DC
-                    another_builder.append(vertex);
-                    angle_intermediate = another_builder.toString();
-                    another_builder.append("] = "+symbols[1]);  // DCB] = ∠[CDE]
-                    System.out.println("∴ ∠["  + another_builder.toString());  // print redundant proof step secondly
-                }
-
-                if (has_sd.containsKey(v.sd)){
-                    builder.append("又 ");
-                    continue;
-                }
-
-                if(count_append == 0){
-                    builder.append("∵ "+v.sd);
-                    /* resolve proof step of lemma */
-                    System.out.println(builder.toString());
-                    builder.setLength(0);
-                }else{
-                    if(count_append == 1){
-                        builder.append("又 ");
-                        builder.append("∵ "+v.sd+"\n");
-                        builder.append( "∴ " + angle_known + " = " + "∠[" + angle_intermediate);
-                    } else{
-                        builder.append(", "+v.sd);
+                    /* detect variant type */
+                    for (String p : primitives_an) {
+                        for (int j = 0; j< 2; j++){
+                            if(p.contains(type_patterns[j]) && j == 1)
+                                is_variant_type = true;
+                        }
                     }
                 }
-                count_append += 1;
+            }
+
+            /* prepare components of proof steps */
+            if (is_variant_type) {
+                if (primitives_an[1].contains(primitives_pn[1])) {
+                    another_builder.append(primitives_an[1].substring(2, 4));
+                } else {
+                    another_builder.append(primitives_an[1].substring(3, 5));
+                }
+                another_builder.reverse();
+                vertex = primitives_an[0].substring(4, 5);
+            } else {
+                if (primitives_an[0].contains(primitives_pn[0])) {
+                    another_builder.append(primitives_an[0].substring(3, 5));
+                } else {
+                    another_builder.append(primitives_an[0].substring(2, 4));
+                }
+                vertex = primitives_an[1].substring(2, 3);
+            }
+            another_builder.append(vertex);
+            angle_intermediate = another_builder.toString();
+            another_builder.append("] = "+primitives_an[1]);  // DCB] = ∠[CDE]
+
+            if(is_reverse_order){
+                for (int i = pr.vlist.size()-1; i >= 0; i--){  // reverse
+                    cond v = (cond) pr.vlist.get(i);
+
+                    if (visited_pr_sd.containsKey(v.sd)){
+                        if(v.sd.contains("∥")){
+                            is_skip_para = true;
+                        }
+                        builder.append("又 ");
+                        continue;
+                    }
+
+                    if(count_append == 0){
+                        builder.append("∵ "+v.sd);
+
+                        if(is_skip_para){
+                            /* resolve proof step of lemma (after skipped parallel) */
+                            System.out.println("∴ ∠["  + another_builder.toString());
+
+                            System.out.println(builder.toString());  // angle
+                            builder.setLength(0);
+
+                            builder.append( "∴ " + angle_known + " = " + "∠[" + angle_intermediate + "]");  // final angles relationship
+                        }else {
+                            System.out.println(builder.toString());  // parallel
+                            builder.setLength(0);
+
+                            /* resolve proof step of lemma (after parallel) */
+                            System.out.println("∴ ∠["  + another_builder.toString());
+                        }
+                    }else{
+                        if(count_append == 1){
+                            builder.append("又 ∵ "+v.sd+"\n");
+                            builder.append( "∴ " + angle_known + " = " + "∠[" + angle_intermediate + "]");  // final angles relationship
+                        } else{
+                            builder.append(", "+v.sd);
+                        }
+                    }
+                    count_append += 1;
+                }
+            }else{
+                for (int i = 0; i < pr.vlist.size(); i++) {
+                    cond v = (cond) pr.vlist.get(i);
+
+                    if (visited_pr_sd.containsKey(v.sd)){
+                        builder.append("又 ");
+                        continue;
+                    }
+
+                    if(count_append == 0){
+                        builder.append("∵ "+v.sd);
+                        /* resolve proof step of lemma */
+                        System.out.println(builder.toString());
+                        System.out.println("∴ ∠["  + another_builder.toString());
+                        builder.setLength(0);
+                    }else{
+                        if(count_append == 1){
+                            builder.append("又 ");
+                            builder.append("∵ "+v.sd+"\n");
+                            builder.append( "∴ " + angle_known + " = " + "∠[" + angle_intermediate + "]");
+                        } else{
+                            builder.append(", "+v.sd);
+                        }
+                    }
+                    count_append += 1;
+                }
             }
         }else{
             for (int i = 0; i < pr.vlist.size(); i++) {
                 cond v = (cond) pr.vlist.get(i);
 
-                if (has_sd.containsKey(v.sd)){
-                    builder.append("又 ");
+                if (visited_pr_sd.containsKey(v.sd)){
+                    is_dup = true;
                     continue;
                 }
 
@@ -91,23 +174,18 @@ public class Main2 {
             }
         }
 
-        System.out.println(builder.toString());
+        String pr_v  = builder.toString();
+        if(is_dup){
+            pr_v = "又 " + pr_v;
+        }
+        System.out.println(pr_v);
 
-        if (!has_sd.containsKey(pr.sd))
-            has_sd.put(pr.sd, 1);
+        if (!visited_pr_sd.containsKey(pr.sd))
+            visited_pr_sd.put(pr.sd, 1);
         System.out.println("∴ "+pr.sd);
     }
 
     public static void main(String[] args) {
-
-//    final public static int CM_EX_PARA = 1;
-//    final public static int CM_EX_ORTH = 2;
-//    final public static int CM_EX_SIMSON = 3;
-//    final public static int CM_EX_SQ = 4;
-//    final public static int CM_EX_PAPPUS = 5;
-//    final public static int CM_EX_PEDAL = 6;
-//    final public static int CM_EX_MIQ1 = 7;
-
         String user_directory = System.getProperty("user.dir");
         String sp = System.getProperty("file.separator");
         // String dr = user_directory + sp + "ex";
@@ -118,31 +196,21 @@ public class Main2 {
         if (result == JFileChooser.CANCEL_OPTION) return;
         gterm gt = new gterm();
         try {
-            // File file_1 = chooser.getSelectedFile();
             // gt.readAterm(new DataInputStream (new FileInputStream(chooser.getSelectedFile())));
             gt.readAterm(new BufferedReader(new FileReader(chooser.getSelectedFile())));
             gib.initRulers();
             Prover.set_gterm(gt);
-            // Prover.run();
             Boolean t = Prover.prove();
             System.out.println(t);
             cond pr_head = Prover.getProveHead();
 
-            Hashtable<String, Integer> has_sd = new Hashtable<String, Integer>();
-            viz_proof(pr_head, has_sd);
+            Hashtable<String, Integer> visited_pr_sd = new Hashtable<String, Integer>();
+            viz_proof(pr_head, visited_pr_sd);
 
             System.out.println(t);
-//            gdd_bc db = new gdd_bc();
-//            db.init_dbase();
-//            db.setExample(gt);
-//            db.sbase();
-//            db.fixpoint();
-//            db.show_fproof();
-
 
         } catch (IOException ee) {
         }
-
         //CMisc.print(Cm.s2077);
     }
 }
